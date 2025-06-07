1617c9eb9b4c4998b6194d08ee0ecdcdb75b54ed
ianthompson11 <ian.thompson@utp.ac.pa>
2025-06-07 16:07:02 -0500

    Modificaciones varias asi como la ceacion inicial del script del player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D ribidBody2D;
+
+    private float inputValue;
+
+    public float moveSpeed = 25;
+
+    private Vector2 direction;
+
+    void Update()
+    {
+
+        
+    }
+}
